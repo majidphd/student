280ae32238eaa2f6a7f23c30eade71f110f23922
majidphd <143480719+majidphd@users.noreply.github.com>
2023-11-09 00:05:35 +0300

    student_predict.py

--- student_predict.py
+++ student_predict.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-import numpy as np
 
 ### --- Page introduction --- By Majed
 st.set_page_config(page_title='Students Prediction')
